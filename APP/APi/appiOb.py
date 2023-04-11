5446d49aa2c3c712317675acbaf864dbe0c6f13e
skycreeds <christovincent2002@gmail.com>
2023-04-11 22:19:18 +0530

    11ap_conti

--- APP/APi/appiOb.py
+++ APP/APi/appiOb.py
@@ -2,10 +2,6 @@ import pandas as pd
 import requests as res
 from Pcomponents import Tools
 class APi:
-    def __init__(self) -> None:
-        self.config=Tools.config_RoE()
-        self.cLient=Client(self.config['api_key'],self.config['api_secret'])
-
     def getminutedata(self,symbol,interval,lookback):
         frame=pd.DataFrame(res.post("https://trade07-1-j3584132.deta.app/",headers={"x-api-key":"a0TvWwDmm4GF_oqheWPNEzJV2RPuzzjPijK6jhvwAoVph"},json={'asset':symbol,'interval':interval,'lookback':lookback}))
         frame=frame.iloc[:,:6]
