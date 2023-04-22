1afa7bc26167f5ea052da9238fb5aa1e4a8859b5
skycreeds <christovincent2002@gmail.com>
2023-04-22 20:44:48 +0530

    ty.

---
+++ APP/APi/DBob.py
@@ -0,0 +1,5 @@
+from deta import Deta
+
+class DBcon:
+    def __init__(self) -> None:
+        self.base=Deta()
--- APP/pages/models.py
+++
@@ -1,5 +0,0 @@
-import streamlit as st
-import pandas as pd
-import requests as res
-
-st.write(pd.DataFrame(res.post("https://trade07-1-j3584132.deta.app/",headers={"x-api-key":"a0TvWwDmm4GF_oqheWPNEzJV2RPuzzjPijK6jhvwAoVph"},json={'asset':'BTCUSDT','interval':'1m','lookback':'1m'}).json()))
