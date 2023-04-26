80553420fe52a16d9a2fc50546889a30835c4797
skycreeds <christovincent2002@gmail.com>
2023-04-26 22:00:00 +0530

    as

--- APP/main.py
+++ APP/main.py
@@ -6,9 +6,8 @@ import streamlit as st
 from APi import DBob
 from Pcomponents.charting import chaRTTY
 import hashlib
-import sys,os
-file_path = '/app/deep_strategy_maker/APP/barfi/'
-sys.path.append(os.path.dirname(file_path))############################################################
+
+############################################################
 
 ###################################################################################
 
