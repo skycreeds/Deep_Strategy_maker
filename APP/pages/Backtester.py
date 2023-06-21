2ad7da718848942421ee6ee5a2aac4705a130a6f
skycreeds <christovincent2002@gmail.com>
2023-06-21 20:14:13 +0530

    sa

--- APP/pages/Backtester.py
+++ APP/pages/Backtester.py
@@ -35,7 +35,7 @@ lookbak=st.number_input('enter lookback  for strategy',min_value=1)
 st.subheader("Time frame is locked to 1m ")
 nummm=st.number_input("lookback in minute for data provided",min_value=5)
 df=api.getminutedata('BTCUSDT','1m',str(nummm)+'m')
-delay=st.number_input('enetr delay animation',min=0)
+delay=st.number_input('enetr delay animation',min_value=0)
 df2=df.copy()
 dflen=len(df2)
 
