8bb7d1bd921d4977751480106d9580f64277bb61
skycreeds <christovincent2002@gmail.com>
2023-08-03 17:55:10 +0530

    as

--- APP/TF_api/TF_es.py
+++ APP/TF_api/TF_es.py
@@ -6,12 +6,12 @@ import os
 @st.cache_resource
 def load_TCN_model():
             print('11111111111111111111111111111111111111111111111',os.getcwd())
-            return tf.keras.models.load_model("/app/deep_strategy_maker/APP/TF_api/TCN_model.h5")
-     
+            return tf.keras.models.load_model("skycreeds/deep_strategy_maker/main/APP/TF_api/TCN_model.h5")
+            #skycreeds/deep_strategy_maker/main/APP/main.py
 @st.cache_resource
 def load_LSTM_model():
       print('11111111111111111111111111111111111111111111111',os.getcwd())
-      return tf.keras.models.load_model("/app/deep_strategy_maker/APP/TF_api/LSTM_model.h5")
+      #return tf.keras.models.load_model("/app/deep_strategy_maker/APP/TF_api/LSTM_model.h5")
 class Tensor_mod:
     def __init__(self) -> None:
           pass
