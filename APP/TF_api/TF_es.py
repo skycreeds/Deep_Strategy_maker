af7f3713083b5896c9b2a55a46f0d0238b503cbd
skycreeds <christovincent2002@gmail.com>
2023-08-03 17:56:56 +0530

    h

--- APP/TF_api/TF_es.py
+++ APP/TF_api/TF_es.py
@@ -6,7 +6,7 @@ import os
 @st.cache_resource
 def load_TCN_model():
             print('11111111111111111111111111111111111111111111111',os.getcwd())
-            return tf.keras.models.load_model("skycreeds/deep_strategy_maker/main/APP/TF_api/TCN_model.h5")
+            return tf.keras.models.load_model("/skycreeds/deep_strategy_maker/main/APP/TF_api/TCN_model.h5")
             #skycreeds/deep_strategy_maker/main/APP/main.py
 @st.cache_resource
 def load_LSTM_model():
