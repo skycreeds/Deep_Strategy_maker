6f73f8b1ad8de6335fc759ae28da4c2977031eb2
skycreeds <christovincent2002@gmail.com>
2023-08-03 16:46:58 +0530

    dfd

--- APP/Pcomponents/Tools.py
+++ APP/Pcomponents/Tools.py
@@ -9,45 +9,6 @@ import requests
 import os
 import sys
 import subprocess
-# #Talib in a Quantitative tading library written in C with python wrappers for fasyer computation of streaming trading data
-# #check if the library folder already exists, to avoid building everytime you load the page
-# if not os.path.isdir("/tmp/ta-lib"):
-
-#     # Download ta-lib to disk
-#     with open("/tmp/ta-lib-0.4.0-src.tar.gz", "wb") as file:
-#         response = requests.get(
-#             "http://prdownloads.sourceforge.net/ta-lib/ta-lib-0.4.0-src.tar.gz"
-#         )
-#         file.write(response.content)
-#     # get our current dir, to configure it back again. Just house keeping
-#     default_cwd = os.getcwd()
-#     os.chdir("/tmp")
-#     # untar
-#     os.system("tar -zxvf ta-lib-0.4.0-src.tar.gz")
-#     os.chdir("/tmp/ta-lib")
-#     # build
-#     os.system("./configure --prefix=/home/appuser/venv/")
-#     os.system("make")
-#     # install
-#     os.system("mkdir -p /home/appuser/venv/")
-#     os.system("make install")
-#     os.system("ls -la /home/appuser/venv/")
-#     # back to the cwd
-#     os.chdir(default_cwd)
-#     sys.stdout.flush()
-
-# # add the library to our current environment
-# from ctypes import *
-
-lib = CDLL("/home/appuser/venv/lib/libta_lib.so.0.0.0")
-# import library
-try:
-    import talib
-except ImportError:
-    subprocess.check_call([sys.executable, "-m", "pip", "install --use-pep517",,"--global-option=build_ext", "--global-option=-L/home/appuser/venv/lib/", "--global-option=-I/home/appuser/venv/include/", "ta-lib==0.4.24"])
-finally:
-    import talib as Tb
-
 import pandas_ta as Tb
 
 def file_join_dir(file):#return the file name relative to current file
